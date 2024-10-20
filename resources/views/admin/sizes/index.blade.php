8a5634754aa3290e5289a836d923884df608944a
Rewal727 <sebastran727@gmail.com>
2024-10-20 20:39:26 +0700

    thua

--- resources/views/admin/sizes/index.blade.php
+++ resources/views/admin/sizes/index.blade.php
@@ -1,15 +1,13 @@
 @extends('adminlte::page')
 
 @section('content')
-    <h1>Sizes</h1>
-    <a href="{{ route('sizes.create') }}" class="btn btn-primary">Add Size</a>
+    <h1>DANH SÁCH KÍCH THƯỚC</h1>
+    <a href="{{ route('sizes.create') }}" class="btn btn-primary">Thêm mới</a>
     <table class="table table-bordered">
         <thead>
             <tr>
                 <th>ID</th>
                 <th>Tên kích thước</th>
-                <th>Chiều dài</th>
-                <th>Chiều rộng</th>
                 <th>Hành động</th>
             </tr>
         </thead>
@@ -18,14 +16,12 @@
                 <tr>
                     <td>{{ $size->id }}</td>
                     <td>{{ $size->name }}</td>
-                    <td>{{ $size->width }}</td>
-                    <td>{{ $size->height }}</td>
                     <td>
-                        <a href="{{ route('sizes.edit', $size->id) }}" class="btn btn-warning">Edit</a>
+                        <a href="{{ route('sizes.edit', $size->id) }}" class="btn btn-warning">Sửa</a>
                         <form action="{{ route('sizes.destroy', $size->id) }}" method="POST" style="display:inline-block;">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
+                            <button type="submit" class="btn btn-danger">Xóa</button>
                         </form>
                     </td>
                 </tr>
